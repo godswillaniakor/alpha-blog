3ec55236daa82a8f5f5b37e9b80f6373f25c2bf0
unknown <godswill.aniakor@gmail.com>
2016-07-14 15:40:53 +0100

    good job

---
+++ app/controllers/say_controller.rb
@@ -0,0 +1,17 @@
+class SayController < ApplicationController
+  def hello
+    @time = Time.now
+  end
+
+  def goodbye  
+  end
+  
+  def filenames   # add the filenames method
+      @files = Dir.glob('*')
+      return @files
+  end
+  
+  def home 
+    
+  end
+end
